9ca56acab91a6d8db90b398286648c42ba4b4937
Filip Szymeczko <szymeczkofil@gmail.com>
2021-09-02 19:10:35 +0200

    Events handler

---
+++ events/interaction-create.js
@@ -0,0 +1,17 @@
+module.exports = {
+  name: 'interactionCreate',
+  execute(interaction) {
+    if (!interaction.isCommand()) return;
+
+    const command = interaction.client.commands.get(interaction.commandName);
+    if (!command) return;
+
+    try {
+      console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an ${interaction.commandName} interaction.`);
+      command.execute(interaction);
+    } catch (error) {
+      console.error(error);
+      interaction.reply({ content: 'Sorki, coś się rozj*bało po mojej stronie', ephemeral: true });
+    }
+  },
+};
--- index.js
+++ index.js
@@ -5,29 +5,20 @@ const { token } = require('./config.json');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 client.commands = new Collection();
 
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
+}
+
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
   client.commands.set(command.data.name, command);
 }
-
-client.once('ready', (c) => {
-  console.log(`Ready! Logged in as ${c.user.tag}`);
-});
-
-client.on('interactionCreate', async interaction => {
-  if (!interaction.isCommand()) return;
-
-  const command = client.commands.get(interaction.commandName);
-  if (!command) return;
-
-  try {
-    console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an ${interaction.commandName} interaction.`);
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-  }
-});
 
 client.login(token);
