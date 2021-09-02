af00a8c1257a0b22161534c79d41dfbe7bba0afb
Filip Szymeczko <szymeczkofil@gmail.com>
2021-09-02 18:57:17 +0200

    Command handler

---
+++ commands/bot.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('bot')
+    .setDescription('Opowiem Ci trochę o sobie!'),
+  async execute(interaction) {
+    await interaction.reply('Jestem Mordeusz!');
+  },
+};
--- index.js
+++ index.js
@@ -1,21 +1,32 @@
-const { Client, Intents } = require('discord.js');
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+client.commands = new Collection();
 
-client.once('ready', () => {
-  console.log('Ready!');
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.data.name, command);
+}
+
+client.once('ready', (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
 client.on('interactionCreate', async interaction => {
   if (!interaction.isCommand()) return;
-  const { commandName } = interaction;
-  if (commandName === 'ping') {
-    await interaction.reply('Pong!');
-  } else if (commandName === 'server') {
-    await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
-  } else if (commandName === 'user') {
-    await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+
+  const command = client.commands.get(interaction.commandName);
+  if (!command) return;
+
+  try {
+    console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an ${interaction.commandName} interaction.`);
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
   }
 });
 
