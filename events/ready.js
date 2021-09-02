f7ee74c857c47c1c5951fa1b3529685d17f4423a
Filip Szymeczko <szymeczkofil@gmail.com>
2021-09-02 23:39:20 +0200

    Test deployment

--- events/ready.js
+++ events/ready.js
@@ -6,5 +6,6 @@ module.exports = {
   execute(client) {
     client.user.setActivity('ofiary', { type: 'WATCHING' });
     console.log(chalk.yellowBright('Ready!'), `Logged in as ${client.user.tag}`);
+    console.log('Deploy test');
   },
 };
