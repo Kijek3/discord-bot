47bb93f6bc3582517993318c398f692c9fd86c03
Filip Szymeczko <szymeczkofil@gmail.com>
2021-09-06 00:07:40 +0200

    feat: settings handler

---
+++ commands/settings.js
@@ -0,0 +1,48 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { Permissions } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('settings')
+    .setDescription('Manage bot settings')
+    .addSubcommandGroup(subcommandGroup =>
+      subcommandGroup
+        .setName('weather')
+        .setDescription('Weather command settings')
+        .addSubcommand(subcommand =>
+          subcommand
+            .setName('config')
+            .setDescription('Config weather command')
+            .addBooleanOption(option => option.setName('enable').setDescription('Should bot send automatic weather?'))
+            .addChannelOption(option => option.setName('channel').setDescription('Set channel for automatic weather'))
+            .addStringOption(option => option.setName('schedule').setDescription('Set schedule for automatic weather in cron format with seconds (* * * * * *)')),
+        )
+        .addSubcommand(subcommand =>
+          subcommand
+            .setName('get')
+            .setDescription('Get specific weather status')
+            .addIntegerOption(option => option.setName('id').setDescription('Weather status id').setRequired(true)),
+        )
+        .addSubcommand(subcommand =>
+          subcommand
+            .setName('remove')
+            .setDescription('Remove specific weather status')
+            .addIntegerOption(option => option.setName('id').setDescription('Record id to remove').setRequired(true)),
+        )
+        .addSubcommand(subcommand =>
+          subcommand
+            .setName('edit')
+            .setDescription('Edit specific weather status')
+            .addIntegerOption(option => option.setName('id').setDescription('Record id to change').setRequired(true))
+            .addStringOption(option => option.setName('new-desc').setDescription('New value status for that record').setRequired(true)),
+        ),
+    ),
+  async execute(interaction) {
+    if (interaction.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
+      const command = require(`./settings/${interaction.options.getSubcommandGroup()}/${interaction.options.getSubcommand()}.js`);
+      await command.execute(interaction);
+    } else {
+      await interaction.reply({ content: 'You must be administrator of this server to use settings', ephemeral: true });
+    }
+  },
+};
