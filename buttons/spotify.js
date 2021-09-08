a7fed41a2a4286a9e0b6d1af0855cdadf07e3946
Filip Szymeczko <szymeczkofil@gmail.com>
2021-09-08 23:43:20 +0200

    feat, refactor: Button collectors

--- buttons/spotify.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: 'spotify',
-  async execute(interaction) {
-    const genre = interaction.customId.replace('spotify-', '');
-    await interaction.reply(genre);
-  },
-};
--- commands/random/spotify.js
+++ commands/random/spotify.js
@@ -3,58 +3,35 @@ const { spotifyToken } = require('../../config.json');
 const { shuffleArray } = require('../../util');
 const { MessageActionRow, MessageButton } = require('discord.js');
 
-module.exports = {
-  async execute(interaction) {
-    const token = await this.getToken();
-    const genres = await this.getGenres(token);
-    const genre = interaction.options.getString('genre');
-    if (genre) {
-      if (genres.includes(genre)) {
-        let albumUrl;
-        while (!albumUrl) {
-          albumUrl = await this.getAlbum(token, genre);
-        }
-        await interaction.reply(albumUrl);
-      } else {
-        interaction.reply({ content: `I never heard about ${genre} genre`, ephemeral: true });
-      }
-      return;
-    }
-    shuffleArray(genres);
-    const row = new MessageActionRow()
-      .addComponents(new MessageButton().setCustomId(`spotify-${genres[0]}`).setLabel(genres[0]).setStyle('PRIMARY'))
-      .addComponents(new MessageButton().setCustomId(`spotify-${genres[1]}`).setLabel(genres[1]).setStyle('PRIMARY'))
-      .addComponents(new MessageButton().setCustomId(`spotify-${genres[2]}`).setLabel(genres[2]).setStyle('PRIMARY'))
-      .addComponents(new MessageButton().setCustomId(`spotify-${genres[3]}`).setLabel(genres[3]).setStyle('PRIMARY'))
-      .addComponents(new MessageButton().setCustomId(`spotify-${genres[4]}`).setLabel(genres[4]).setStyle('PRIMARY'));
-    interaction.reply({ content: 'Choose your genre', components: [ row ], ephemeral: true });
-  },
-  async getToken() {
-    let token;
-    await fetch('https://accounts.spotify.com/api/token', {
-      method: 'POST',
-      headers: {
-        Authorization: `Basic ${spotifyToken}`,
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: 'grant_type=client_credentials',
-    }).then(res => res.json())
-      .then(resToken => token = resToken.access_token);
-    return token;
-  },
-  async getGenres(token) {
-    let genres = [];
-    await fetch('https://api.spotify.com/v1/recommendations/available-genre-seeds', {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    }).then(res => res.json())
-      .then(resGenres => genres = resGenres.genres);
-    return genres;
-  },
-  async getAlbum(token, genre) {
-    let albumUrl;
+async function getToken() {
+  let token;
+  await fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    headers: {
+      Authorization: `Basic ${spotifyToken}`,
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: 'grant_type=client_credentials',
+  }).then(res => res.json())
+    .then(resToken => token = resToken.access_token);
+  return token;
+}
+
+async function getGenres(token) {
+  let genres = [];
+  await fetch('https://api.spotify.com/v1/recommendations/available-genre-seeds', {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }).then(res => res.json())
+    .then(resGenres => genres = resGenres.genres);
+  return genres;
+}
+
+async function getAlbumUrl(token, genre) {
+  let album;
+  while (!album || !album[0]) {
     await fetch(`https://api.spotify.com/v1/recommendations?limit=1&seed_genres=${genre}`, {
       method: 'GET',
       headers: {
@@ -62,7 +39,51 @@ module.exports = {
         'Content-Type': 'application/json',
       },
     }).then(res => res.json())
-      .then(resTrack => albumUrl = resTrack?.tracks[0]?.album?.external_urls?.spotify);
-    return albumUrl;
+      .then(resTrack => album = resTrack?.tracks);
+  }
+  const albumUrl = album[0]?.album?.external_urls?.spotify;
+  return albumUrl;
+}
+
+module.exports = {
+  async execute(interaction) {
+    const token = await getToken();
+    const genres = await getGenres(token);
+    let genre = interaction.options.getString('genre');
+
+    if (genre) {
+      if (genres.includes(genre)) {
+        let albumUrl;
+        while (!albumUrl) {
+          albumUrl = await getAlbumUrl(token, genre);
+        }
+        await interaction.reply(albumUrl);
+      } else {
+        interaction.reply({ content: `I never heard about ${genre} genre`, ephemeral: true });
+      }
+      return;
+    }
+
+    shuffleArray(genres);
+    const row = new MessageActionRow();
+    for (let i = 0; i < 5; i++) {
+      row.addComponents(new MessageButton().setCustomId(`${genres[i]}`).setLabel(genres[i]).setStyle('PRIMARY'));
+    }
+    await interaction.reply({ content: 'Choose your genre', components: [ row ] });
+
+    const message = await interaction.fetchReply();
+    const collector = message.createMessageComponentCollector({ componentType: 'BUTTON', time: 15000 });
+    collector.on('collect', async (i) => {
+      if (i.user.id === interaction.user.id) {
+        genre = i.customId;
+        let albumUrl;
+        while (!albumUrl) {
+          albumUrl = await getAlbumUrl(token, genre);
+        }
+        await interaction.editReply({ content: albumUrl, components: [ ] });
+      } else {
+        await i.reply({ content: 'These buttons aren\'t for you!', ephemeral: true });
+      }
+    });
   },
 };
