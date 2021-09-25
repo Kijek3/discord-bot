c976bab1927aec2055ab0247d12e7a1f08b11214
Filip Szymeczko <szymeczkofil@gmail.com>
2021-09-25 14:49:35 +0200

    refactor: move cronjob to function

--- events/ready.js
+++ events/ready.js
@@ -1,36 +1,40 @@
 const CronJob = require('cron').CronJob;
 const { sequelize, Weather, Settings } = require('../db-objects.js');
 
+async function scheduleWeather(client) {
+  const schedule = await Settings.findOne({
+    where: { setting: 'weather_schedule' },
+  });
+  const job = new CronJob(schedule.value, async () => {
+    const enabled = await Settings.findOne({
+      where: { setting: 'weather_enable' },
+    });
+    if (enabled.value) {
+      const resp = await Weather.findOne({
+        order: sequelize.random(),
+      });
+      if (resp) {
+        const channelId = await Settings.findOne({
+          where: { setting: 'weather_channel' },
+        });
+        if (channelId.value) {
+          const channel = client.channels.cache.get(channelId.value);
+          channel.send(resp.description);
+        }
+      }
+    }
+  });
+
+  job.start();
+}
+
 module.exports = {
   name: 'ready',
   once: true,
   async execute(client) {
+    scheduleWeather(client);
+
     client.user.setActivity('ofiary', { type: 'WATCHING' });
     console.log(`Ready! Logged in as ${client.user.tag}`);
-
-    const schedule = await Settings.findOne({
-      where: { setting: 'weather_schedule' },
-    });
-    const job = new CronJob(schedule.value, async () => {
-      const enabled = await Settings.findOne({
-        where: { setting: 'weather_enable' },
-      });
-      if (enabled.value) {
-        const resp = await Weather.findOne({
-          order: sequelize.random(),
-        });
-        if (resp) {
-          const channelId = await Settings.findOne({
-            where: { setting: 'weather_channel' },
-          });
-          if (channelId.value) {
-            const channel = client.channels.cache.get(channelId.value);
-            channel.send(resp.description);
-          }
-        }
-      }
-    });
-
-    job.start();
   },
 };
